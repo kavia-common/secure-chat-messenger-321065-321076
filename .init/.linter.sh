e7eac0a646eec8e2a876efc1a39d91598c8339ed
Kavia AI <production@kavia.ai>
2026-02-16 09:48:14 +0000

    Checkpoint 1 - cgbffbf274

---
+++ .init/.linter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/secure-chat-messenger-321065-321076/messenger_backend
+dotnet build --no-restore -v quiet -nologo -consoleloggerparameters:NoSummary /p:TreatWarningsAsErrors=false
+LINT_EXIT_CODE=$?
+if [ $LINT_EXIT_CODE -ne 0 ]; then
+  exit 1
+fi
+
